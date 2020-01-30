977332b80706c0b3ba9a869d79035bcc4dbb371d
Leti <lgsainz15@gmail.com>
2020-01-30 17:31:57 -0500

    set up function to generate a new board

---
+++ src/PlinkoGame.java
@@ -0,0 +1,47 @@
+public class PlinkoGame {
+
+    /**
+     * Play a round of Plinko.
+     */
+    public static void main(String[] args) {
+        String[][] board = generateBoard(5,5);
+        // printBoard(board);
+        // dropPin(2);
+    }
+
+    /**
+     * Generate new board of dimensions row x col with alternating L, R.
+     */
+    public static String[][] generateBoard(int rows, int cols) {
+        String[][] board = new String[rows][cols];
+        boolean toggle = true;
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                toggle = !toggle;
+                if (toggle) {
+                    board[i][j] = "L";
+                }
+                else {
+                    board[i][j] = "R";
+                }
+            }
+        }
+        return board;
+    }
+
+    /**
+     * Print the board to the console.
+     */
+    public static void printBoard(String[][] board) {
+
+    }
+
+    /**
+     * Drop the pin into the board at a given column.
+     */
+    public static void dropPin(String[][] board, int col) {
+
+    }
+
+}
