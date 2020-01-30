df2dc2bdf930a913aca49ae75c3922aaa7cefa37
Leti <lgsainz15@gmail.com>
2020-01-30 17:33:04 -0500

    added function to print out board to the console

--- src/PlinkoGame.java
+++ src/PlinkoGame.java
@@ -5,7 +5,7 @@ public class PlinkoGame {
      */
     public static void main(String[] args) {
         String[][] board = generateBoard(5,5);
-        // printBoard(board);
+         printBoard(board);
         // dropPin(2);
     }
 
@@ -34,7 +34,12 @@ public class PlinkoGame {
      * Print the board to the console.
      */
     public static void printBoard(String[][] board) {
-
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 
     /**
