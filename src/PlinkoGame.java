a02093911fe4d18d862c56c23fc80fe8e5167523
Leti <lgsainz15@gmail.com>
2020-01-31 15:10:55 -0500

    added dropChip method that prints out visual of chip moving through board

--- src/PlinkoGame.java
+++ src/PlinkoGame.java
@@ -4,14 +4,15 @@ public class PlinkoGame {
      * Play a round of Plinko.
      */
     public static void main(String[] args) {
-        generateBoard(5,5);
-//        dropPin(2);
+        PlinkoGame game = new PlinkoGame();
+        String[][] board = game.generateBoard(5,5);
+        game.dropChip(board, 1);
     }
 
     /**
      * Generate new board of dimensions row x col with alternating L, R.
      */
-    public static void generateBoard(int rows, int cols) {
+    public String[][] generateBoard(int rows, int cols) {
         String[][] board = new String[rows][cols];
         boolean toggle = true;
 
@@ -27,12 +28,14 @@ public class PlinkoGame {
             }
         }
         printBoard(board);
+        System.out.println();
+        return board;
     }
 
     /**
      * Print the board to the console.
      */
-    public static void printBoard(String[][] board) {
+    public void printBoard(String[][] board) {
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[i].length; j++) {
                 System.out.print(board[i][j] + " ");
@@ -44,8 +47,33 @@ public class PlinkoGame {
     /**
      * Drop the pin into the board at a given column.
      */
-    public static void dropPin(String[][] board, int col) {
+    public void dropChip(String[][] board, int col) {
+        String chip = "o";
+        int row = 0;
 
+        System.out.println(board.length);
+
+        while (row < board.length) {
+//            System.out.println("before: " + board[row][col] + row);
+
+            if (board[row][col] == "L" && col > 0) {
+                board[row][col] = chip;
+                col--;
+            }
+            else if (board[row][col] == "R" && col < board[row].length - 1) {
+                board[row][col] = chip;
+                col++;
+            }
+            else {
+                board[row][col] = chip;
+            }
+            printBoard(board);
+            System.out.println();
+            row++;
+
+//            System.out.println("after: " + board[row][col] + row);
+//            System.out.println(col);
+        }
     }
 
 }
